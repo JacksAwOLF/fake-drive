eceb914244467caef5b4e1cf41f2e1d386a3a4c2
Chiango Chen <chiangochen@gmail.com>
2024-08-21 11:06:02 -0700

    scss and basic layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <>
-      <div class="header">
-        
-      </div>
-      <div class="dragarea">
-      
+      <div class="container">
+        <div class="sidebar">
+          
+        </div>
+        <div class="header">
+          
+        </div>
+        <div class="dragarea">
+
+        </div>
       </div>
     </>
   );
